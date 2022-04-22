b77f7d63dcbf9201bd98257f579fdc2f186d7117
Maximilien Noal <noal.maximilien@gmail.com>
2022-04-22 22:28:07 +0200

    Initiate multiplatform UI with Avalonia
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Aeon.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Aeon.Avalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
