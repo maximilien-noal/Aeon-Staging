381a6e05a2ecd94e412f3986e5f2a69e3c7b80ef
Maximilien Noal <noal.maximilien@gmail.com>
2022-04-24 09:30:34 +0200

    Aeon.Avalonia: Basic blocks
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Aeon.Avalonia/ViewModels/ViewModelBase.cs
+++ src/Aeon.Avalonia/ViewModels/ViewModelBase.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using ReactiveUI;
 
-namespace Aeon.Avalonia.ViewModels
+namespace Aeon.Avalonia.ViewModels;
+
+public class ViewModelBase : ReactiveObject
 {
-    public class ViewModelBase : ReactiveObject
-    {
-    }
 }
