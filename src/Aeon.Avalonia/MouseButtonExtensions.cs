2ddb143bb450be15f32c51ffeb5060bd2d45baac
Maximilien Noal <noal.maximilien@gmail.com>
2022-04-24 10:35:56 +0200

    Aeon.Avalonia: more basic blocks
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Aeon.Avalonia/MouseButtonExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+using Avalonia.Input;
+
+namespace Aeon.Emulator.Launcher
+{
+    /// <summary>
+    /// Contains extension methods for the Avalonia.Input.MouseButton type.
+    /// </summary>
+    public static class MouseButtonExtensions
+    {
+        /// <summary>
+        /// Returns an Aeon.Emulator.MouseButtons value from a Avalonia.Input.MouseButton value.
+        /// </summary>
+        /// <param name="mouseButton">Avalonia.Input.MouseButton value to convert.</param>
+        /// <returns>Aeon.Emulator.MouseButtons value.</returns>
+        public static MouseButtons ToEmulatorButtons(this MouseButton mouseButton)
+        {
+            return mouseButton switch
+            {
+                MouseButton.Left => MouseButtons.Left,
+                MouseButton.Middle => MouseButtons.Middle,
+                MouseButton.Right => MouseButtons.Right,
+                _ => MouseButtons.None,
+            };
+        }
+    }
+}
