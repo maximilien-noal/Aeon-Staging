b6e7f5d0edf4bb7bad4bf6df7d01df209bef670b
Maximilien Noal <noal.maximilien@gmail.com>
2022-05-01 17:48:55 +0200

    Aeon.Emulator: Linux specific fixes to GraphicsPresenter8...
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Aeon.Emulator/Video/Rendering/GraphicsPresenter8.cs
+++ src/Aeon.Emulator/Video/Rendering/GraphicsPresenter8.cs
@@ -25,45 +25,22 @@ namespace Aeon.Emulator.Video.Rendering
             var palette = this.VideoMode.Palette;
             byte* srcPtr = (byte*)this.VideoMode.VideoRam.ToPointer() + (uint)this.VideoMode.StartOffset;
             uint* destPtr = (uint*)destination.ToPointer();
-            /*
-            for (int i = 0; i < totalPixels; i++)
-            {
-                var value = srcPtr[i];
-                destPtr[i] = palette[value];
-            }
-            */
-            //RowBytes=2560
-            //Pixelize=640x400
+
             var height = this.VideoMode.Height;
             var width = this.VideoMode.Width;
             var offset = 0;
-            for (int row = 0; row < height; row++)
+            for (int y = 0; y < height; y++)
             {
-                uint* startOfLine = destPtr;
-                uint* endOfLine = destPtr + width;
-                for (uint* column = startOfLine; column < endOfLine; column++)
+                uint* startPtr = destPtr + offset;
+                uint* endPtr = destPtr + offset + width;
+                for (uint* x = startPtr; x < endPtr; x++)
                 {
                     var src = srcPtr[offset];
                     var pixel = palette[src];
-                    *column = ToArgb(pixel);
+                    *x = ToArgb(pixel);
                     offset++;
                 }
-                destPtr += width;
             }
-        }
-        public uint ToRgba(uint pixel) {
-            var color = Color.FromArgb((int)pixel);
-            return (uint)(color.R << 16 | color.G << 8 | color.B) | 0xFF000000;
-        }
-
-        public uint ToBgra(uint pixel) {
-            var color = Color.FromArgb((int)pixel);
-            return (uint)(color.B << 16 | color.G << 8 | color.R) | 0xFF000000;
-        }
-
-        public uint ToArgb(uint pixel) {
-            var color = Color.FromArgb((int)pixel);
-            return 0xFF000000 | ((uint)color.R << 16) | ((uint)color.G << 8) | color.B;
         }
     }
 }
